45df8294cd55f5a0a43c97fe2640ba8bc0c1f582
samuelcoding97 <samuel.coop.miller@gmail.com>
2023-04-22 18:12:14 -0700

    created frames for the home page

--- ui.py
+++ ui.py
@@ -1,7 +1,11 @@
 import tkinter as tk
 
 window = tk.Tk()
-greeting = tk.Label(text="Hello World")
-greeting.pack()
+
+header_frame = tk.Frame(master=window, width=1250, height=175, bg='cyan')
+header_frame.pack()
+
+body_frame = tk.Frame(master=window, width=1250, height=900, bg='light green')
+body_frame.pack()
 
 window.mainloop()
