872a704bee3752500c22886de3e61441699195c3
samuelcoding97 <samuel.coop.miller@gmail.com>
2023-04-22 18:54:34 -0700

    created the home button with colors and fonts

--- ui.py
+++ ui.py
@@ -1,11 +1,25 @@
-import tkinter as tk
+from tkinter import *
 
-window = tk.Tk()
+window = Tk()
 
-header_frame = tk.Frame(master=window, width=1250, height=175, bg='cyan')
-header_frame.pack()
+header_frame1 = Frame(master=window, height=25, bg='cyan')
+header_frame1.pack(fill=X)
+header_frame2 = Frame(master=window, height=0, bg='cyan')
+header_frame2.pack(fill=X)
+header_button = Button(
+    master=header_frame2,
+    text="Spanish Verb Practice",
+    font=('Arial', 35),
+    relief=GROOVE,
+    borderwidth=5,
+    width=0,
+    height=0,
+    bg='cyan')
+header_button.pack()
+header_frame3 = Frame(master=window, height=25, bg='cyan')
+header_frame3.pack(fill=X)
 
-body_frame = tk.Frame(master=window, width=1250, height=900, bg='light green')
-body_frame.pack()
+body_frame = Frame(master=window, height=900, bg='light green')
+body_frame.pack(fill=X)
 
 window.mainloop()
